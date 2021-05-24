43d91eaaf72ab028613a18ec5e0209992b92ddaf
mundostr <idux.net@gmail.com>
2021-05-24 17:42:26 -0300

    agregado sumatoria

---
+++ sumatoria.py
@@ -0,0 +1,7 @@
+sumatoria = 0
+
+for ciclo in range (5):
+	ingreso = int(input("Ingresar nro: "))
+	sumatoria = sumatoria + ingreso # puede escribirse también como sumatoria += ingreso
+
+print(sumatoria)
