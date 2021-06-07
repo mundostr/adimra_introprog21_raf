496d57d9beb9964e6ba8bde9f9c69f884b84edc2
mundostr <idux.net@gmail.com>
2021-06-07 17:34:33 -0300

    Ronda Personas simplificado

--- practicas/ronda_personas.py
+++ practicas/ronda_personas.py
@@ -1,35 +1,31 @@
-import random # importamos la librería random para poder generar nros al azar
+LIMITE_CUENTA = 100
+CANTIDAD_PERSONAS = 10
 
-CTD_PERSONAS = 10
-LIMITE_CUENTA = 25
+cuenta = 0
+persona = 0
+sentidoGiro = "horario"
 
-persona = random.randint(0, CTD_PERSONAS - 1) # solo utilizamos un nro random para ver qué persona comienza el conteo
-sentidoRonda = "horario"
+for ciclo in range(LIMITE_CUENTA):
+	# Incremento de cuenta
+	cuenta += 1 # cuenta = cuenta + 1
 
-for cuenta in range(1, LIMITE_CUENTA + 1):
-	if (sentidoRonda == "horario"):
-		if (persona < CTD_PERSONAS):
+	# Control de persona dentro del límite de la ronda
+	if (sentidoGiro == "horario"):
+		if (persona < CANTIDAD_PERSONAS):
 			persona = persona + 1
 		else:
-			persona = 1 # no podemos superar CTD_PERSONAS, si persona no es <, vuelve a 1
+			persona = 1
 	else:
 		if (persona > 1):
 			persona = persona - 1
 		else:
-			persona = CTD_PERSONAS # no podemos bajar de 1, si persona no es > 1, vuelve a CTD_PERSONAS
-
-	if (cuenta % 8 == 0): # verificamos si cuenta es perfectamente divisible por 8 p/ cambiar el sentido de giro
-		if (sentidoRonda == "horario"):
-			sentidoRonda = "antihorario"
-		else:
-			sentidoRonda = "horario"
+			persona = CANTIDAD_PERSONAS
 	
-	if (cuenta % 12 == 0):
-		# La intención es verificar si cuenta es perfectamente divisible por 11, no obstante utilizamos 12 en el if
-		# para que el cálculo de persona se realice antes del salto, caso contrario quedaría desfazado por 1
-		if (sentidoRonda == "horario"):
-			persona = persona + 1
+	# Control de cuenta perfectamente divisible por 8
+	if (cuenta % 8 == 0):
+		if (sentidoGiro == "horario"):
+			sentidoGiro = "antihorario"
 		else:
-			persona = persona - 1
-
-	print("sentido:", sentidoRonda, " | persona:", persona, " | cuenta:", cuenta)
+			sentidoGiro = "horario"
+	
+	print(cuenta, persona, sentidoGiro)
